ccc57162dafe8dc74dfefb33ff9ade4a881735ac
Cory Perkins <link2cory@gmail.com>
2019-07-31 09:11:37 -0700

    activity get method now returns real data from the database

--- services/activity/project/api/activity.py
+++ services/activity/project/api/activity.py
@@ -1,6 +1,6 @@
+from flask import jsonify
 from flask_restful import Resource, fields, marshal_with
 
-import project.db as db 
 from .post_parser import parse_args
 from project.model import Activity as Model
 
@@ -12,7 +12,7 @@ activity_response_fields = {
 
 class Activity(Resource):
     def get(self):
-        return {'activity': 'Doing a Thing'}
+        return jsonify(Model.objects)
     
     @marshal_with(activity_response_fields)
     def post(self):
