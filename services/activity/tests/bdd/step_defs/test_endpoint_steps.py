b70ce30807a66a91356e524ded0aa068ff844fa7
Cory Perkins <link2cory@gmail.com>
2019-11-10 14:47:00 -0800

    extracted the 'extra types' dictionary out of the decorator parser

--- services/activity/tests/bdd/step_defs/test_endpoint_steps.py
+++ services/activity/tests/bdd/step_defs/test_endpoint_steps.py
@@ -6,10 +6,14 @@ import requests
 
 scenarios('../features/endpoint.feature')
 
+EXTRA_TYPES = {
+    'Number': int,
+}
+
 @given('the activity api is queried with no params')
 def response(docker_web):
   return requests.get(docker_web)
 
-@then(parsers.parse('the response status code is "{code:Number}"', extra_types=dict(Number=int)))
+@then(parsers.parse('the response status code is "{code:Number}"', extra_types=EXTRA_TYPES))
 def response_code(response, code):
   assert response.status_code == code
