7857b02358b45839afa23aeab9cf91b197ad2fc4
Cory Perkins <link2cory@gmail.com>
2019-11-10 18:35:56 -0800

    added simple functional test that checks that the response of a get request to the activity endpoint contains data from the database

--- services/activity/tests/bdd/features/api_get.feature
+++ services/activity/tests/bdd/features/api_get.feature
@@ -3,6 +3,12 @@ Feature: Activity Endpoint
   I want to receive information about activities I have posted
   So that I can keep track of how I am spending my time
 
-  Scenario: Simple get request
+  Scenario: Simple get request with no data
     Given the activity api is queried with no params
     Then the response status code is "200"
+
+  Scenario: Simple get request with data
+    Given there is an activity record in the database
+    And the activity api is queried with no params
+    Then the response status code is "200"
+    And the response contains the aforementioned activity record
--- services/activity/tests/bdd/step_defs/test_api_get_steps.py
+++ services/activity/tests/bdd/step_defs/test_api_get_steps.py
@@ -14,6 +14,31 @@ EXTRA_TYPES = {
 def response(docker_web):
   return requests.get(docker_web)
 
+@given('there is an activity record in the database')
+def activity_record(docker_web):
+    activity_record = {'name': 'testing'}
+    # create a record by POSTing to the endpoint
+    requests.post(docker_web, activity_record)
+
+    return activity_record
+
 @then(parsers.parse('the response status code is "{code:Number}"', extra_types=EXTRA_TYPES))
 def response_code(response, code):
   assert response.status_code == code
+
+@then('the response contains the aforementioned activity record')
+def response_content(response, activity_record):    
+    record_found = False
+
+    # check if there is a record contained in the response that has the same
+    # data as the activity_record passed
+    for response_record in response.json():
+        print(response_record)
+        print(activity_record)
+        # the response record will have extra fields so check for subset 
+        # rather than equivalence
+        if response_record.items() >= activity_record.items():
+            record_found = True
+            break
+    assert record_found
+
